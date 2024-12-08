7f7171a161164861cd5efae81cd7f2b9233e354d
Damir Zhenisbek <damircodenn@gmail.com>
2024-12-08 22:07:26 +0500

    Added createAsynkThunk

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import createBookWithID from '../../utils/createBookWithID'
 import booksData from '../../data/books.json'
-import { addBook, thunkFunction } from '../../redux/slices/booksSlice'
+import { addBook, fetchBook } from '../../redux/slices/booksSlice'
 import './BookForm.css'
 
 const BookForm = () => {
@@ -27,7 +27,7 @@ const BookForm = () => {
   }
 
   const handleAddRandomBookViaAPI = () => {
-    dispatch(thunkFunction)
+    dispatch(fetchBook())
   }
 
   return (
